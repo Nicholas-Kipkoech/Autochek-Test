8e111aa8a0f79429be5643ee2d6ffe816ad73c56
Nicholas-Kipkoech <nickey968@gmail.com>
2022-07-31 16:50:17 +0300

    fetching data from api

--- autochek-frontend/pages/Navbar.tsx
+++ autochek-frontend/pages/Navbar.tsx
@@ -1,22 +1,30 @@
-const Nav =()=>{
-  return (  <div>
-    <nav className="navbar navbar-expand-lg bg-light">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Cars</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <form className="d-flex" role="search">
-        <input className="form-control me-2 flex-end" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-danger bg-red" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
-  </div>
-  )
-}
-
+const Nav = () => {
+  return (
+    <nav className="navbar bg-light">
+      <div className="container">
+        <a className="navbar-brand" href="#">
+          <img
+            src="https://media.autochek.africa/file/publicAssets/full-color-logo-new.png"
+            alt=""
+            width="150"
+            height="40"
+            className="d-inline-block align-text-top"
+          />
+          <form className="d-flex" role="search">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </a>
+      </div>
+    </nav>
+  );
+};
 
 export default Nav;
